2dff2a37a930e6d09b5636dda95184c956a09730
Seweryn Kuliga <skuligas@gmail.com>
2021-11-10 12:17:54 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <Body>asd
+      
+    </Body>
+  );
+}
+
+export default App;
+
+const Body = styled.div`
+
+`
